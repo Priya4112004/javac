2343cebee3dc7b126b2b6f9b9f203a670b592019
Priya4112004 <158718706+Priya4112004@users.noreply.github.com>
2024-02-15 10:30:18 +0530

    Create boxArea.h

---
+++ week3/boxArea.h
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+ void boxArea(float length, float width,float height){
+    cout<<"Area of the box is :"<<2(length*width+width*height+length*height);
+ }
+ include<iostream>
+#include"boxArea.h"
+using namespace std;
+int main(){
+    boxArea(1.2,1.2,1.3);
+}    
